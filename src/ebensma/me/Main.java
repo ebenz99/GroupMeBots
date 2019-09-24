f92733895533bd061958463b2b79b27ada9731b4
Ethan Bensman <ebenz99@gmail.com>
2019-09-24 13:31:24 -0400

    Got access token

--- src/ebensma/me/Main.java
+++ src/ebensma/me/Main.java
@@ -1,9 +1,25 @@
 package ebensma.me;
+import java.io.*;
+import java.util.Properties;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	    System.out.println("Hello World");
+	    Properties prop = getKey();
+	    String access_token = prop.getProperty("access_token");
+        System.out.println(access_token);
+    }
 
+    public static Properties getKey() throws IOException {
+        Properties prop=new Properties();
+        FileInputStream fin= new FileInputStream("/Users/ebensman/IdeaProjects/GroupMeBots/config.properties");
+        prop.load(fin);
+        return prop;
+    }
+
+    public static void auth(){
+        //https://oauth.groupme.com/oauth/authorize?client_id=+clientid
+        int a = 1;
     }
 }
