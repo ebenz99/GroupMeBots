534ce8fa4d5df2218ac8effa465e5775c5c6dc08
Ethan Bensman <ebenz99@gmail.com>
2019-09-24 13:02:50 -0400

    Adding gitignore

--- src/ebensma/me/Main.java
+++ src/ebensma/me/Main.java
@@ -3,6 +3,7 @@ package ebensma.me;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World from Java");
+	    System.out.println("Hello World");
+
     }
 }
