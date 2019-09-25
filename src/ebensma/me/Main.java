00ff7579db1b0737a58a5e49402ffdfabc52aebe
Ethan Bensman <ebenz99@gmail.com>
2019-09-25 13:07:29 -0400

    Slight Changes

--- src/ebensma/me/Main.java
+++ src/ebensma/me/Main.java
@@ -5,13 +5,13 @@ import java.util.Properties;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	    System.out.println("Hello Worldd");
-	    Properties prop = getKey();
+	    System.out.println("Hello Worlddd");
+	    Properties prop = getKeys();
 	    String access_token = prop.getProperty("access_token");
         System.out.println(access_token);
     }
 
-    public static Properties getKey() throws IOException {
+    public static Properties getKeys() throws IOException {
         Properties prop=new Properties();
         FileInputStream fin= new FileInputStream("/Users/ebensman/IdeaProjects/GroupMeBots/config.properties");
         prop.load(fin);
